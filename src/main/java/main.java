975cbb7914da7c0728dbc01009cbedb9111f9db0
Iudhi <iudhicunhasantos0@gmail.com>
2026-03-27 19:49:39 -0300

    adição do while, try e catch para criação de loop em caso de resultado inesperado

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,16 +1,34 @@
+
 import javax.swing.JOptionPane;
+
 public class main {
 
     public static void main(String[] args) {
-        String nome; double peso;
-        JOptionPane.showMessageDialog(null,"vou divinhar seu peso! ");
-        nome = JOptionPane.showInputDialog(null,"Digite seu nome: ");
-        peso = Double.parseDouble(JOptionPane.showInputDialog(null,"Digite seu peso: "));
+        String nome = "";
+        double peso = 0;
+        String input = null;
+        boolean EntradaValida = false;
+        while (!EntradaValida) {
+            JOptionPane.showMessageDialog(null, "vou divinhar seu peso! ");
+            nome = JOptionPane.showInputDialog(null, "Digite seu nome: ");
+            input = JOptionPane.showInputDialog(null, "Digite seu peso: ");
+            //em caso de cancelamento
+            if (nome == null) {
+                JOptionPane.showMessageDialog(null, "Operação cancelada.");
+                return;
+            }
         
-        if(peso <= 0) {
-            JOptionPane.showMessageDialog(null,"Mentirosooo!");
-        } else {
-             JOptionPane.showMessageDialog(null,"Olá! "+ peso + "Kg" + " seu peso é: " + nome);
+        try {
+            peso = Double.parseDouble(input);
+            if (peso <= 0) {
+                JOptionPane.showMessageDialog(null, "Mentirosooo!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Olá! " + peso + "Kg" + " seu peso é: " + nome);
+            }
+            EntradaValida = true;
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Digite um número válido!");
+        }
         }
     }
 }
